3d628322b1c4651b50eb15da715a78f4d1e2b8d9
LUKAS DE LIMA SODRE <20242014040020@ifrn.local>
2026-05-12 13:34:52 -0300

    chore(settings): config db e installed apps (transporte_app, drf_yasg, rest_framework)

--- transporte_api/transporte_api/settings.py
+++ transporte_api/transporte_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'transporte_app',
+    'drf_yasg',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'transporte_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'transporte_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
